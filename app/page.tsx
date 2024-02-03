032a01e6290514277cb2f2d8688d584b3df1f24d
subikcnc <subik@curvesncolors.ch>
2024-02-03 21:40:40 +0545

    Clerk Authentication added

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS 14. We&apos;re coming</h1>
-      <h2 className="h2-bold">NextJS 14</h2>
-    </div>
-  );
-}
