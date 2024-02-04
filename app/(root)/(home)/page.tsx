a68c5ca377956046fc64d848b0a964bb7b24db67
subikcnc <subik@curvesncolors.ch>
2024-02-04 12:14:06 +0545

    testing ssh connection

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen">
-      <h1>This is home screen</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
