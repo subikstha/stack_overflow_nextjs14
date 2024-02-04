b6cafab13da571f97df38c1571789baada7929a5
subikcnc <subik@curvesncolors.ch>
2024-02-04 11:50:24 +0545

    checking github username

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen">
+      <h1>This is home screen</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
