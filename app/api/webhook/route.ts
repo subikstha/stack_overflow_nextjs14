379cedde07af49510f8fb6d5ec3e98bf9a387b3e
subikstha <subsatch@gmail.com>
2024-04-02 22:47:15 +0545

    changed webhook_secret in env

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,7 +8,7 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   //   TODO: Add webhook secret to .env.local file
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
